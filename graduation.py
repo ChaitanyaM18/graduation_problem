3cfba7f88a4219988e2fd74fa663771234a41ef7
Chaitanya <47985883+ChaitanyaM18@users.noreply.github.com>
2021-10-25 17:01:38 +0530

    Update graduation.py

--- graduation.py
+++ graduation.py
@@ -12,6 +12,6 @@ def attend_graduation(n):
             no_of_ways.append(i)
             if abs_.endswith("A"):
                 not_attend.append(i)
-    no_to_attend = len(no_of_ways)
-    prob = str(len(not_attend))+"/"+str(len(no_of_ways))
+    no_to_attend = len(no_of_ways)  #The number of ways to attend classes over N days.
+    prob = str(len(not_attend))+"/"+str(len(no_of_ways)) #The probability that you will miss your graduation ceremony.
     return no_to_attend,prob
