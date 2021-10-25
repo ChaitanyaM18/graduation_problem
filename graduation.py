c628f7238a817088dbb354319b37afdb92935fa7
chaitanya <chaitanyamaralihalli18@gmail.com>
2021-10-25 16:58:06 +0530

    graduation solution

---
+++ graduation.py
@@ -0,0 +1,17 @@
+import itertools
+
+def attend_graduation(n):
+    attendance = list(itertools.product("AP", repeat=n))
+    no_of_ways = []
+    not_attend = []
+    absent_str = 'AAAA'
+
+    for i in attendance:
+        abs_ = "".join(i)
+        if absent_str not in abs_:
+            no_of_ways.append(i)
+            if abs_.endswith("A"):
+                not_attend.append(i)
+    no_to_attend = len(no_of_ways)
+    prob = str(len(not_attend))+"/"+str(len(no_of_ways))
+    return no_to_attend,prob
